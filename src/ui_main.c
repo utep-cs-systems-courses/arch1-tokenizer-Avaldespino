ec88558efb750d93df5c43a5c5c5a28bd22e8518
Adrian Valdespino <avaldespino@miners.utep.edu>
2021-02-16 20:26:27 -0700

    Worked on count words

--- src/ui_main.c
+++ src/ui_main.c
@@ -6,11 +6,25 @@ int main(){
   puts("Please type your string then press enter:");
 
   
-  char str[100];
+   char str[100];
+  
+   fgets(str,100,stdin);
+   puts(str);
+   int words;
+   words = 0;
+  words = count_words(str);
+   printf("Word count: %d\n",words);
+  
   
 
-  fgets(str,100,stdin);
-  puts(str);
+  
+    
+    
+    //printf("%d\n",str[i]);
+      
+    // printf("for loop is runing");
+    
+  
 
   
  
